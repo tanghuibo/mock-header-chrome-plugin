eea6b8827d4a9aa303ff75e36991ef0cb2e279be
tang_huibo <1215893264@qq.com>
2020-05-20 00:06:00 +0800

    完成修改 header 功能

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
-console.log("mock-header-chrome-plugin init");
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function (request) {
+    let requestHeaders = request.requestHeaders;
+    requestHeaders.push({
+      name: "mock-test",
+      value: '{"username": "thb"}',
+    });
+    return { requestHeaders };
+  },
+  {
+    urls: ["<all_urls>"],
+    types: ["xmlhttprequest"],
+  },
+  ["requestHeaders", "blocking"]
+);
